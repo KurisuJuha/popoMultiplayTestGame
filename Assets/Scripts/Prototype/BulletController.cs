844f20a2fbb32176288b5e6dd4996b1be2068efd
KYapp <kukiyomenaisapp@gmail.com>
2023-03-05 00:39:42 +0900

    create bulletController

---
+++ Assets/Scripts/Prototype/BulletController.cs
@@ -0,0 +1,22 @@
+using JuhaKurisu.PopoTools.Utility;
+using JuhaKurisu.PopoTools.Extentions;
+using AnnulusGames.LucidTools.Inspector;
+using UnityEngine;
+
+[HideMonoScript]
+public class BulletController : PopoBehaviour
+{
+    [ShowInInspector, TabGroup("Tab", "Parameter")] public bool isMyTeam { get; private set; }
+    [SerializeField, TabGroup("Tab", "Parameter")] private float speed;
+    [SerializeField, Required, TabGroup("Tab", "Components")] private Rigidbody2D rigidBody2D;
+
+    protected override void Update()
+    {
+        rigidBody2D.velocity = transform.up * speed;
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!(other.TryGetComponent<BulletController>(out var otherBullet) && otherBullet.isMyTeam)) Destroy(gameObject);
+    }
+}
