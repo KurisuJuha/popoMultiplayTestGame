337548f799207901f66939f9bd25069f27c3f670
KYapp <kukiyomenaisapp@gmail.com>
2023-03-05 16:00:09 +0900

    弾の発射処理

--- Assets/Scripts/Prototype/GunController.cs
+++ Assets/Scripts/Prototype/GunController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using JuhaKurisu.PopoTools.Extentions;
 using JuhaKurisu.PopoTools.Utility;
 using AnnulusGames.LucidTools.Inspector;
 
@@ -7,8 +8,15 @@ public class GunController : PopoBehaviour
 {
     [SerializeField, Required, TabGroup("Tab", "Components")] private Transform gunTransform;
     [SerializeField, TabGroup("Tab", "Parameter")] private float maxGunDistance;
+    [SerializeField, TabGroup("Tab", "Parameter"), DisableInPlayMode] private float interval;
     [SerializeField, TabGroup("Tab", "Parameter"), ReadOnly] private Vector3 cursorPosition;
+    [SerializeField, Required, TabGroup("Tab", "Prefabs")] private GameObject bulletPrefab;
+    private ActionWithInterval FireWithInterval;
 
+    protected override void Awake()
+    {
+        FireWithInterval = new ActionWithInterval(interval, Fire);
+    }
 
     protected override void Update()
     {
@@ -16,6 +24,7 @@ public class GunController : PopoBehaviour
         cursorPosition = new(cursorPosition.x, cursorPosition.y, 0);
 
         RotateGun();
+        if (Input.GetKey(KeyCode.Space)) FireWithInterval.Invoke();
     }
 
     private void RotateGun()
@@ -29,5 +38,12 @@ public class GunController : PopoBehaviour
 
         RaycastHit2D hit2D = Physics2D.Raycast(transform.position, transform.right, maxGunDistance, 1 << 9);
         gunTransform.localPosition = new(hit2D ? ((Vector2)transform.position - hit2D.point).magnitude : maxGunDistance, 0, 0);
+    }
+
+    private void Fire()
+    {
+        "Fire".Inspect();
+
+        Instantiate(bulletPrefab, gunTransform.position, Quaternion.Euler(0, 0, gunTransform.rotation.eulerAngles.z - 90));
     }
 }
